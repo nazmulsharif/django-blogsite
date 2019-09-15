820d4e3e7874a2b56ff3baed7574047f22ff383b
nazmul <nazmulict94@gmail.com>
2019-09-15 11:00:56 +0600

    Blog

---
+++ blog_post/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+from .models import Post
+
+
+class BlogPpostConfig(AppConfig):
+    name = 'blog_post'
---
+++ blog_post/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import Post
+def home(request):
+    all_post = Post.objects.all()
+    return render(request, 'index.html',{'all_post_list': all_post})
